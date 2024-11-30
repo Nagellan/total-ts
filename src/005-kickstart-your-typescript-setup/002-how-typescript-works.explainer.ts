41cc5dab3b60636a0a6e3ebb8aa9dc016cd47b84
Nagellan <ireknazmievirek@gmail.com>
2024-11-30 16:44:53 +0400

    Complete 005.002

--- src/005-kickstart-your-typescript-setup/002-how-typescript-works.explainer.ts
+++ src/005-kickstart-your-typescript-setup/002-how-typescript-works.explainer.ts
@@ -1,1 +1,14 @@
 // Explain diagram
+
+/**
+ * the JavaScript-only diagram shows that the code written by user in JS
+ * may then be run directly at the JS runtime environments: browser or nodejs.
+ *
+ * the TypeScript diagram is different from the JavaScript-only diagram,
+ * because the code written by user cannot be run directly at the JS runtime
+ * environments, because it's not JS! There have to be an additional process
+ * that transforms code in TypeScript into the code on JavaScript, and only then
+ * this code may be run. Having an additional process, we pay an extra for
+ * the opportunity to access the powerful type system that helps to write
+ * a better and safer code.
+ */
