faf03393e6d3c8969750acfdb0524cc1a087b450
Nagellan <ireknazmievirek@gmail.com>
2024-11-30 16:52:18 +0400

    Complete 005.004

--- src/005-kickstart-your-typescript-setup/004-set-up-node-js-and-vscode.explainer.ts
+++ src/005-kickstart-your-typescript-setup/004-set-up-node-js-and-vscode.explainer.ts
@@ -1,1 +1,6 @@
 // Explain in video
+
+/**
+ * Matt shows how to install both VSCode IDE and NodeJS JavaScript runtime environment
+ * and how to make sure everything is installed correctly and works properly.
+ */
