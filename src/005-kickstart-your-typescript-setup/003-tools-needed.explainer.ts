84cab816976ad017af0e1fc450a276ad0226125e
Nagellan <ireknazmievirek@gmail.com>
2024-11-30 16:49:58 +0400

    Complete 005.003

--- src/005-kickstart-your-typescript-setup/003-tools-needed.explainer.ts
+++ src/005-kickstart-your-typescript-setup/003-tools-needed.explainer.ts
@@ -1,1 +1,12 @@
 // Explain diagram
+
+/**
+ * The diagram shows the complete development cycle: from writing the code
+ * and to the code processing. The process starts from the IDE that provides
+ * us the interface for accessing, creating and modifying the code.
+ * Then this code is transformed from TypeScript into the JavaScript by
+ * the TypeScript CLI in order to make it possible to run the written code.
+ * Finally, code in JavaScript is then run by the JavaScript runtime
+ * environment. This process repeats over and over again in the development
+ * loop that happens while the project is being developed.
+ */
