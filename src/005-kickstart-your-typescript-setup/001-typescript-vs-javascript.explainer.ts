8ddae421a642e16d650b096307b79d48d74c2c52
Nagellan <ireknazmievirek@gmail.com>
2024-11-30 16:32:35 +0400

    Complete 005.001

--- src/005-kickstart-your-typescript-setup/001-typescript-vs-javascript.explainer.ts
+++ src/005-kickstart-your-typescript-setup/001-typescript-vs-javascript.explainer.ts
@@ -1,5 +1,31 @@
 // Explain autocomplete and type checking
 
+/**
+ * Autocomplete happens when user modifies code, typescript server watches the changes,
+ * analyzes the modified code, looks at the types behind that code and its constructions,
+ * checks possible constructions use and suggests them to be applied to the code.
+ *
+ * Type checking analyzes the provided code on possible mistakes. The code constructions
+ * are checked on accordance to the type system rules. If there are any errors or warnings,
+ * they are shown to the user.
+ */
+
 // Check your types are correct across your whole application
 
+/**
+ * Taking into account that this directory is my whole application for now,
+ * I have run the following command:
+ *
+ * ```
+ * node_modules/typescript/bin/tsc
+ * ```
+ *
+ * I have got the following output summary:
+ * "Found 297 errors in 151 files."
+ */
+
 // Refactor with confidence
+
+/**
+ * I will, thanks
+ */
