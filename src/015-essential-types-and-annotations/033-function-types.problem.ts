bd94db92c4a92fa0c4625dfcd50286dc3c3600c1
Nagellan <ireknazmievirek@gmail.com>
2024-12-08 16:22:04 +0400

    Complete 015.033

--- src/015-essential-types-and-annotations/033-function-types.problem.ts
+++ src/015-essential-types-and-annotations/033-function-types.problem.ts
@@ -3,7 +3,9 @@ type User = {
   name: string;
 };
 
-const modifyUser = (users: User[], id: string, makeChange) => {
+type MakeChange = (user: User) => User;
+
+const modifyUser = (users: User[], id: string, makeChange: MakeChange) => {
   return users.map((u) => {
     if (u.id === id) {
       return makeChange(u);
@@ -27,5 +29,5 @@ modifyUser(
   // @ts-expect-error
   (user) => {
     return { ...user, name: 123 };
-  },
+  }
 );
