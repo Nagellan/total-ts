3fdf5253db142b01f8507c0c75bc428c8ce1dd8f
Nagellan <ireknazmievirek@gmail.com>
2024-12-08 15:23:34 +0400

    Fix 015.022

--- src/015-essential-types-and-annotations/022-all-types.problem.ts
+++ src/015-essential-types-and-annotations/022-all-types.problem.ts
@@ -1,5 +1,5 @@
 let example1: string = "Hello World!";
 let example2: number = 42;
 let example3: boolean = true;
-let example4: Symbol = Symbol();
-let example5: BigInt = 123n;
+let example4: symbol = Symbol();
+let example5: bigint = 123n;
