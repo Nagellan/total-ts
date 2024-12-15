5b3cc1446d9b6d1875a1a2b28a0a6082788f4917
Nagellan <ireknazmievirek@gmail.com>
2024-12-15 18:29:46 +0400

    Complete 016.5.051

--- src/016.5-ide-superpowers/051-prettier-format-on-save.explainer.ts
+++ src/016.5-ide-superpowers/051-prettier-format-on-save.explainer.ts
@@ -1,5 +1,26 @@
 // Explain prettier
 
+/**
+ * Prettier formats files instead of user's manual edits.
+ * It uses a configurations file or its default configurations and applies
+ * those formatting rules into the code on some trigger action such as
+ * manual command run or format on save action.
+ */
+
 // Explain how to set up prettier
 
+/**
+ * In order to work, prettier needs to be installed either globally or in workspace.
+ * After the installation, you can access the prettier command in terminal and just use
+ * the cli to format the files. You may also set up a prettier configuration file in order
+ * to define same formatting rules for the whole repository, especially for those
+ * working on it not alone. If you use VSCode, you can install Prettier extension to
+ * access features integrated into an IDE, such as save on format.
+ */
+
 // Explain how to set up formatOnSave
+
+/**
+ * To set up formatOnSave, the one need to open VSCode settings, type formatOnSave to
+ * the searchbox, and check the checkbox.
+ */
