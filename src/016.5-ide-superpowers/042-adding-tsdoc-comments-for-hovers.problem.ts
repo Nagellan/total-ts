7ed1094a12f9981b4588f4fb1780395d46f3711b
Nagellan <ireknazmievirek@gmail.com>
2024-12-15 17:27:02 +0400

    Complete 016.5.042

--- src/016.5-ide-superpowers/042-adding-tsdoc-comments-for-hovers.problem.ts
+++ src/016.5-ide-superpowers/042-adding-tsdoc-comments-for-hovers.problem.ts
@@ -1,3 +1,10 @@
+/**
+ * Summarizes two passed numbers.
+ *
+ * @param a first number
+ * @param b second number
+ * @returns sum of first and second numbers
+ */
 const myFunction = (a: number, b: number) => {
   return a + b;
 };
