f5b0d6a03b2563bf526a97ab1865d59105e3acb0
Nagellan <ireknazmievirek@gmail.com>
2024-12-15 17:55:53 +0400

    Complete 016.5.050

--- src/016.5-ide-superpowers/050-refactor.problem.ts
+++ src/016.5-ide-superpowers/050-refactor.problem.ts
@@ -1,5 +1,12 @@
 const func = () => {
   // Refactor this to be its own function
-  const randomPercentage = `${(Math.random() * 100).toFixed(2)}%`;
+  const randomPercentage = getRandomPercentage();
   console.log(randomPercentage);
 };
+
+function getRandomPercentage() {
+  return `${getRandomFixedNumber(2)}%`;
+}
+function getRandomFixedNumber(fractionDigits: number) {
+  return (Math.random() * 100).toFixed(fractionDigits);
+}
